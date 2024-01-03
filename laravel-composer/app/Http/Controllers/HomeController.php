b12deff3992f105368ae58d937af535d0f9c4203
buituanloc <tuanloc29042000@gmail.com>
2024-01-03 20:24:20 +0700

    update

---
+++ laravel-composer/app/Http/Controllers/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class HomeController extends Controller
+{
+//    public function changeLanguage($language)
+//    {
+//        if (! in_array($language, ['en', 'vi'])) {
+//            abort(400);
+//        }
+//        App::setLocale($language);
+//        return view('welcome');
+//    }
+    public function changeLanguage(Request $request,$language)
+    {
+        if (! in_array($language, ['en', 'vi'])) {
+            abort(400);
+        }
+        $request->session()->put(['lang' => $language]);
+        return redirect()->back();
+    }
+
+}
